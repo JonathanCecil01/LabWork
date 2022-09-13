f6ee8e77c0d044154b66c6e721fe68f347d6707c
Jonathan Cecil <82434403+JonathanCecil01@users.noreply.github.com>
2022-09-13 09:05:54 +0000

    commit 1

--- DesignAnalyseAlgo/Kruskals.cpp
+++ DesignAnalyseAlgo/Kruskals.cpp
@@ -1,1 +1,37 @@
 #include <iostream>
+#include <vector>
+#define edge pair<int, int>
+
+using namespace std;
+
+class Graph
+{ 
+    private:
+        vector<pair<int, edge> > G;  // graph
+        vector<pair<int, edge> > T;  // mst
+        int *parent;
+        int V;  // number of vertices/nodes in graph
+    public:
+        Graph(int V);
+        void AddWeightedEdge(int u, int v, int w);
+        int find_set(int i);
+        void union_set(int u, int v);
+        void kruskal();
+        void print();
+};
+
+Graph::Graph(int V)
+{
+    this->V = V;
+}
+void Graph::AddWeightedEdge(int u, int v, int w)
+{
+    
+}
+
+
+int main()
+{
+    cout<<"CodeSpaceRocks"<<endl;
+    return 0;
+}
