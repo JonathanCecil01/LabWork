63977ad67cf0e7af74e307dbaf026a7bb35b37bc
Jonathan Cecil <82434403+JonathanCecil01@users.noreply.github.com>
2022-09-17 06:32:51 +0000

    commit 2

--- DesignAnalyseAlgo/jobsequence.cpp
+++ DesignAnalyseAlgo/jobsequence.cpp
@@ -2,50 +2,36 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-
-// A structure to represent a job
 struct Job {
 
-	char id; // Job Id
-	int dead; // Deadline of job
-	int profit; // Profit if job is over before or on
-				// deadline
-};
-
-// Comparator function for sorting jobs
+	char id; 
+	int dead;
+	int profit; 
 bool comparison(Job a, Job b)
 {
 	return (a.profit > b.profit);
 }
-
-// Returns maximum profit from jobs
 void printJobScheduling(Job arr[], int n)
 {
-	// Sort all jobs according to decreasing order of profit
 	sort(arr, arr + n, comparison);
 
-	int result[n]; // To store result (Sequence of jobs)
-	bool slot[n]; // To keep track of free time slots
+	int result[n]; 
+	bool slot[n]; 
 
-	// Initialize all slots to be free
 	for (int i = 0; i < n; i++)
 		slot[i] = false;
 
-	// Iterate through all given jobs
 	for (int i = 0; i < n; i++) {
-		// Find a free slot for this job (Note that we start
-		// from the last possible slot)
+		
 		for (int j = min(n, arr[i].dead) - 1; j >= 0; j--) {
 			// Free slot found
 			if (slot[j] == false) {
-				result[j] = i; // Add this job to result
-				slot[j] = true; // Make this slot occupied
+				result[j] = i;
+				slot[j] = true;
 				break;
 			}
 		}
 	}
-
-	// Print the result
 	for (int i = 0; i < n; i++)
 		if (slot[i])
 			cout << arr[result[i]].id << " ";
@@ -64,7 +50,6 @@ int main()
 	cout << "Following is maximum profit sequence of jobs "
 			"\n";
 
-	// Function call
 	printJobScheduling(arr, n);
 	return 0;
 }
