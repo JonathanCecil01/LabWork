769cd0d7dfe09019170c0967b863a6f4daf73fda
Jonathan Cecil <82434403+JonathanCecil01@users.noreply.github.com>
2022-09-17 06:28:45 +0000

    job sequence final commit

--- DesignAnalyseAlgo/jobsequence.cpp
+++ DesignAnalyseAlgo/jobsequence.cpp
@@ -1,57 +1,55 @@
-// C program for the above approach
+// C++ code for the above approach
 
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-typedef struct Job {
+#include <algorithm>
+#include <iostream>
+using namespace std;
 
-	char id; 
-	int dead; 
-	int profit; 
-} Job;
+// A structure to represent a job
+struct Job {
 
+	char id; // Job Id
+	int dead; // Deadline of job
+	int profit; // Profit if job is over before or on
+				// deadline
+};
 
-int compare(const void* a, const void* b)
+// Comparator function for sorting jobs
+bool comparison(Job a, Job b)
 {
-	Job* temp1 = (Job*)a;
-	Job* temp2 = (Job*)b;
-	return (temp2->profit - temp1->profit);
+	return (a.profit > b.profit);
 }
 
-int min(int num1, int num2)
-{
-	return (num1 > num2) ? num2 : num1;
-}
-
+// Returns maximum profit from jobs
 void printJobScheduling(Job arr[], int n)
 {
-	qsort(arr, n, sizeof(Job), compare);
+	// Sort all jobs according to decreasing order of profit
+	sort(arr, arr + n, comparison);
 
-	int result[n]; 
-	bool slot[n]; 
+	int result[n]; // To store result (Sequence of jobs)
+	bool slot[n]; // To keep track of free time slots
 
-	
+	// Initialize all slots to be free
 	for (int i = 0; i < n; i++)
 		slot[i] = false;
 
+	// Iterate through all given jobs
 	for (int i = 0; i < n; i++) {
-	
-	
+		// Find a free slot for this job (Note that we start
+		// from the last possible slot)
 		for (int j = min(n, arr[i].dead) - 1; j >= 0; j--) {
-		
-			
+			// Free slot found
 			if (slot[j] == false) {
-				result[j] = i;
-				slot[j] = true;
+				result[j] = i; // Add this job to result
+				slot[j] = true; // Make this slot occupied
 				break;
 			}
 		}
 	}
 
-	
+	// Print the result
 	for (int i = 0; i < n; i++)
 		if (slot[i])
-			printf("%c ", arr[result[i]].id);
+			cout << arr[result[i]].id << " ";
 }
 
 // Driver's code
@@ -62,10 +60,12 @@ int main()
 				{ 'c', 2, 27 },
 				{ 'd', 1, 25 },
 				{ 'e', 3, 15 } };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	printf(
-		"Following is maximum profit sequence of jobs \n");
 
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "Following is maximum profit sequence of jobs "
+			"\n";
+
+	// Function call
 	printJobScheduling(arr, n);
 	return 0;
 }
